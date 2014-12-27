f17cfcc53fd89f2b535294158edefa3d433a770f
Andy <andrew.sunglae.kim@gmail.com>
2014-12-27 15:27:27 -0500

    modified create functionality

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -2,4 +2,29 @@ class DealsController < ApplicationController
 	def index
 		@deals = Deal.all
 	end
+
+	def show
+		@deal = Deal.find(params[:id])
+	end
+
+	def new
+		@vendors = Vendor.all
+		@vendors_options = []
+		@items = Item.all
+		@deal = Deal.new
+	end
+
+	def create
+		@deal = Deal.new
+		if @deal.save
+			redirect_to @deal
+		else render 'new'
+		end
+	end
+
+	private
+	def deal_params
+		params.require(:deal).permit(:price)
+	end
+
 end
