c9db79c1436047ed6ac714b8c3415e6a9f5c0fe2
Andy <andrew.sunglae.kim@gmail.com>
2015-01-06 11:01:33 -0500

    removed sqlite3

--- Gemfile
+++ Gemfile
@@ -44,9 +44,7 @@ gem 'responders', '~> 2.0'
 group :production do
   gem 'pg'
 end
-group :development, :test do
-  gem 'sqlite3'
-end
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141228011310) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "deals", force: :cascade do |t|
     t.integer  "item_id"
     t.integer  "vendor_id"
@@ -53,8 +56,8 @@ ActiveRecord::Schema.define(version: 20141228011310) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "vendors", force: :cascade do |t|
     t.string   "name"
