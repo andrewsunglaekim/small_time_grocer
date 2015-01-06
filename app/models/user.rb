e7598d733ebfccdaca0d82c01c07f80cadb8fc93
Andy <andrew.sunglae.kim@gmail.com>
2015-01-06 10:14:29 -0500

    change sqlite3 to postgresql

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,16 @@
 class User < ActiveRecord::Base
+	has_and_belongs_to_many :vendors 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.from_omniauth(auth)
+		where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+		  user.provider = auth.provider
+		  user.uid = auth.uid
+		  user.email = auth.info.email
+		  user.password = Devise.friendly_token[0,20]
+		end
+  end
 end
--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,4 +1,5 @@
 class Vendor < ActiveRecord::Base
+	has_and_belongs_to_many :users
 	has_many :deals, dependent: :destroy
 	has_many :items, :through => :deals
 	validates :name, uniqueness: true
